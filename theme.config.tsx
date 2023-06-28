3cafdd38aede4406f6b457f5e78cce56bf85ea26
iamleomoko <mokoliyolo@gmail.com>
2023-06-28 14:54:59 +0200

    Adding structure

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span><b>Ideas</b></span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/firstelevenorg/ideas',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/Dpvx8dzuPg',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/firstelevenorg/ideas',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Ideas',
   },
 }
 
